74ddafe72aa2bfe2b130e35d3cb29a4668de85be
crissavino <csavino@padelmanager.com>
2020-05-27 16:48:02 +0200

    add toggle class for show or not all the tags

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -7,11 +7,19 @@
         <section id="main" class="wrapper style1 fullscreen fade-up">
             <div class="inner">
                 <a href="{{route('blog')}}"><h2>Articulos publicados</h2></a>
-                    <ul class="actions small row d-inline-flex">
-                        @foreach ($tags as $tag)
-                            <li><a href="{{route('blog', ['tagTitle' => $tag->title])}}" class="button primary small mt-3">{{$tag->title}}</a></li>
-                        @endforeach
-                    </ul>
+                <div class="tags">
+                    <button id="toggleTags" class="btn-block mb-3" type="button">
+                        Categorias
+                    </button>
+                    <div id="collapsedTags" class="collapse">
+                        <ul class="actions small row d-inline-flex">
+                            @foreach ($tags as $tag)
+                                <li><a href="{{route('blog', ['tagTitle' => $tag->title])}}" class="button primary small mt-3">{{$tag->title}}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+                <hr>
                 <div class="features" id="blogContainer">
                     @foreach ($articles as $article)
                         <section class="d-block w-100">
@@ -33,3 +41,13 @@
     </div>
 @endsection
 
+@section('javascript')
+    <script>
+      const tagButton = document.getElementById('toggleTags')
+      const collapsedTags = document.getElementById('collapsedTags')
+
+      tagButton.addEventListener('click', () => {
+        collapsedTags.classList.toggle('show')
+      })
+    </script>
+@stop
