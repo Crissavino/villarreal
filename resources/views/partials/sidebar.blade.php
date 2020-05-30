e579605a7723a6d933f58716aa795311a9a56ac7
crissavino <csavino@padelmanager.com>
2020-05-30 10:49:49 +0200

    some fixes v2

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,9 +1,13 @@
 <section id="sidebar">
+    <div class="marca">
+        <span class="text-wrapper">
+            <span class="letters letters-left">V</span>
+            <span class="letters ampersand">&amp;</span>
+            <span class="letters letters-right">V</span>
+          </span>
+    </div>
+
     <div class="inner">
-        <div class="marca">
-            Villarreal <br>
-            Abogados
-        </div>
 
         <nav>
             <ul>
@@ -31,8 +35,10 @@
         <nav id="hamburgerNav">
             <ul id="hamburgerUL">
                 <li id="hamburgerLI"><a href="{{route('index')}}">Bienvenido</a></li>
-                <li id="hamburgerLI"><a onclick="goToIndexTwo()" href="{{route('index')}}#two">Quienes somos?</a></li>
-                <li id="hamburgerLI"><a onclick="goToIndexOne()" href="{{route('index')}}#one">Ultimos articulos</a></li>
+                <li id="hamburgerLI"><a onclick="goToIndexTwo()" href="{{route('index')}}#two">Quienes somos?</a>
+                </li>
+                <li id="hamburgerLI"><a onclick="goToIndexOne()" href="{{route('index')}}#one">Ultimos articulos</a>
+                </li>
                 <li id="hamburgerLI"><a href="{{route('contacto')}}">Contactanos</a></li>
                 @auth
                     @if (auth()->user()->isAdmin)
@@ -47,3 +53,39 @@
         </nav>
     </div>
 </section>
+
+@section('javascript')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+
+    <script>
+      anime.timeline({loop: false})
+      .add({
+        targets: '.marca .line',
+        opacity: [0.5,1],
+        scaleX: [0, 1],
+        easing: "easeInOutExpo",
+        duration: 1000
+      }).add({
+        targets: '.marca .ampersand',
+        opacity: [0, 1],
+        scaleY: [0.5, 1],
+        easing: "easeInOutExpo",
+        duration: 800,
+        offset: '-=800'
+      }).add({
+        targets: '.marca .letters-left',
+        opacity: [0, 1],
+        translateX: ["2em", 0],
+        easing: "easeInOutExpo",
+        duration: 800,
+        offset: '-=300'
+      }).add({
+        targets: '.marca .letters-right',
+        opacity: [0, 1],
+        translateX: ["-2em", 0],
+        easing: "easeInOutExpo",
+        duration: 800,
+        offset: '-=800'
+      })
+    </script>
+@endsection
